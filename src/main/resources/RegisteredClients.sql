d8737058ffa537095cb06e5e51bcbddbcf3d92d0
jihed.chaabane <jihed.chaabanee@gmail.com>
2025-05-28 13:24:05 +0200

    ..

--- src/main/resources/RegisteredClients.sql
+++ src/main/resources/RegisteredClients.sql
@@ -1,3 +1,5 @@
+sudo -u postgres psql -d postgres_data_source
+
 CREATE TABLE IF NOT EXISTS oauth2_registered_client (
    id VARCHAR(255) NOT NULL,
    client_id VARCHAR(255) NOT NULL,
@@ -36,7 +38,7 @@ INSERT INTO oauth2_registered_client (
     'products-client',
     CURRENT_TIMESTAMP,
     '$2a$10$9jqiljibjNsTOLHadkmbJupbJ54lnSQ0UvZcsvmkMcLbgoon0F7de', -- BCrypt pour "secret"
-    'products-client-name',
+    'products-client--name',
     'client_secret_basic',
     'client_credentials',
     'products.read,products.write',
@@ -67,7 +69,7 @@ INSERT INTO oauth2_registered_client (
     'client1',
     CURRENT_TIMESTAMP,
     '$2a$10$7kPsZCgc5FnK.CZFiugblO8eqOHll1WZymm9PHY4D6fMGarvnd5fC', -- BCrypt pour "secret1"
-    'client1-name',
+    'client1--name',
     'client_secret_basic',
     'client_credentials',
     'client1.read,client1.write,client2.read,client2.write',
@@ -98,7 +100,7 @@ INSERT INTO oauth2_registered_client (
     'client2',
     CURRENT_TIMESTAMP,
     '$2a$10$w4GclNTUKApSCDnrgxkibunO2oSiq9VLyv3z8XHdl263Mdc5Wfkjq', -- BCrypt pour "secret2"
-    'client2-name',
+    'client2--name',
     'client_secret_basic',
     'client_credentials',
     'client2.read,client2.write',
@@ -190,7 +192,7 @@ INSERT INTO oauth2_registered_client (
     '6',
     'gr-ms3-resource',
     CURRENT_TIMESTAMP,
-    '$2a$10$VNqId.GQaTNZDCVou25ItunFldfKKcShq2FTJJ5axHLcwJ.R7z006', -- BCrypt pour "ms3-resource"
+    '$2a$10$RE2zxeKwHxSEtLc.VSUcYeQMxsY6X/Yr3yBffob./yGkNhFvs9/iO', -- BCrypt pour "ms3-resource"
     'gr-ms3-resource--name',
     'client_secret_basic',
     'client_credentials',
@@ -360,25 +362,27 @@ INSERT INTO oauth2_registered_client (
 	  "settings.token.refresh-token-time-to-live": ["java.time.Duration", 3600.000000000]
 	}'
 );
+----==> La valeur 1800.000000000 correspond à 30 minutes (1800 secondes).
+----==> Ajustez selon les besoins (par exemple, 3600.000000000 pour 1 heure).
 
 select id, client_id, scopes, client_name
 from oauth2_registered_client
-order by id;
-
+order by id::NUMERIC;
  id |             client_id             |                                       scopes                                       |               client_name
 ----+-----------------------------------+------------------------------------------------------------------------------------+-----------------------------------------
- 1  | products-client                   | products.read,products.write,client1.read,client1.write,client2.read,client2.write | products-client-name
- 10 | gr-oauth2-swagger-ms1             | read,write                                                                         | gr-oauth2-swagger-ms1--name
- 11 | gr-oauth2-swagger-ms2             | update,remove                                                                      | gr-oauth2-swagger-ms2--name
- 2  | client1                           | client1.read,client1.write,client2.read,client2.write                              | client1-name
- 3  | client2                           | client2.read,client2.write                                                         | client2-name
+ 1  | products-client                   | products.read,products.write,client1.read,client1.write,client2.read,client2.write | products-client--name
+ 2  | client1                           | client1.read,client1.write,client2.read,client2.write                              | client1--name
+ 3  | client2                           | client2.read,client2.write                                                         | client2--name
  4  | gr-ms1-resource                   | ms1.read,ms2.read                                                                  | gr-ms1-resource--name
  5  | gr-ms2-resource                   | ms2.read                                                                           | gr-ms2-resource--name
- 6  | gr-ms6-resource                   | ms3.read,actuator.read                                                             | gr-ms3-resource--name
+ 6  | gr-ms3-resource                   | ms3.read,actuator.read                                                             | gr-ms3-resource--name
  7  | gr-resource-consumer-resttemplate | ms1.read,ms2.read,ms3.read                                                         | gr-resource-consumer-resttemplate--name
  8  | gr-resource-consumer-webclient    | ms1.read,ms2.read,ms3.read                                                         | gr-resource-consumer-webclient--name
  9  | gr-resource-consumer-feign        | ms1.read,ms2.read,ms3.read                                                         | gr-resource-consumer-feign--name
+ 10 | gr-oauth2-swagger-ms1             | read,write                                                                         | gr-oauth2-swagger-ms1--name
+ 11 | gr-oauth2-swagger-ms2             | update,remove                                                                      | gr-oauth2-swagger-ms2--name
 (11 lignes)
+
 
 
 gr-ms1-resource:ms1-resource=> $2a$10$DknUlal8WNAMOM8JN2EAcuaqKLsvUNIovApYiP5rfwmOZI...VhI6
@@ -388,7 +392,7 @@ gr-ms1-resource:ms1-resource=> $2a$10$DknUlal8WNAMOM8JN2EAcuaqKLsvUNIovApYiP5rfw
 gr-ms2-resource:ms2-resource=> $2a$10$39cfG2df23kkU4.RJGi33ua5E0IR/gEpWxfCL/LcwBwcNZ9XHJpWm
 .scope("ms2.read")
 
-gr-ms3-resource:ms3-resource=> $2a$10$VNqId.GQaTNZDCVou25ItunFldfKKcShq2FTJJ5axHLcwJ.R7z006
+gr-ms3-resource:ms3-resource=> $2a$10$RE2zxeKwHxSEtLc.VSUcYeQMxsY6X/Yr3yBffob./yGkNhFvs9/iO
 .scope("ms3.read")
 .scope("actuator.read")
 
